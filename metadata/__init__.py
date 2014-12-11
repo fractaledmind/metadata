f6ad28142bf97b0b8e1342541babbb678da36cb0
Stephen Margheim <margheim@sas.upenn.edu>
2014-12-11 13:57:38 -0500

    bump version

--- metadata/__init__.py
+++ metadata/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 __title__ = 'metadata'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Stephen Margheim'
 __license__ = 'MIT'
 __copyright__ = 'Copyright © 2014 Stephen Margheim'
